787460be74bd70e949972514229f8b20215a540d
Zain Imtiaz <90681723+zenimtiazz@users.noreply.github.com>
2022-02-28 09:39:10 +0100

    test for B-Dawg key chain works

--- Python/test_gilded_rose.py
+++ Python/test_gilded_rose.py
@@ -9,8 +9,19 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("foo", items[0].name)
+        self.assertEqual("foo", items[0].name)
+   
+    def test_B_DAWG(self):
+        items = [Item("B-DAWG Keychain", 20, 80)]
+        gilded = GildedRose(items)
+        gilded.update_quality()
+        self.assertEqual(80, items[0].quality)
+      
+    def test_BDAWG_past_date(self):
+        items = [Item("B-DAWG Keychain", 0, 80)]
+        gilded = GildedRose(items)
+        gilded.update_quality()
+        self.assertEquals(80, items[0].quality)  
 
-        
 if __name__ == '__main__':
     unittest.main()
